584f35be3b0924ec27517db80ce3a32e55d9bfe4
DAppArtisan <141224058+DAppArtisan@users.noreply.github.com>
2025-01-10 13:26:00 +0530

    super chat ui

--- src/components/aichat/chatfeed/ChatFeed.tsx
+++ src/components/aichat/chatfeed/ChatFeed.tsx
@@ -1,6 +1,8 @@
 import { useEffect, useRef, useState } from "react";
 import "./chatfeed.scss";
 import VirtualizedContainer from "../../common/virtualList.tsx";
+
+import closeIcon from "../../../assets/close.svg";
 import socket from "../../../services/socket.ts";
 import UserMessage from "../userMessage/index.tsx";
 export default function ChatFeed() {
@@ -12,24 +14,7 @@ export default function ChatFeed() {
   const [isAutoScrollEnabled, setIsAutoScrollEnabled] = useState(true);
   const loadingArray = Array(5).fill(() => 0);
 
-  // const fetchData = async () => {
-  //   setIsLoading(true);
-  //   try {
-  //     const response = await fetch(
-  //       `https://jsonplaceholder.typicode.com/posts?_page=${page}&_limit=${limit}`
-  //     );
-  //     const data = await response.json();
-  //     console.log("DATA", data);
-  //     setChat((prevChat) => [...prevChat, ...data]);
-  //   } catch (error) {
-  //     console.error("Error fetching data:", error);
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // };
-  // useEffect(() => {
-  //   fetchData();
-  // }, [page]);
+  const [isFullHeight, setIsFullHeight] = useState(true);
 
   useEffect(() => {
     // if wallet add succesfull then start connect
@@ -50,78 +35,88 @@ export default function ChatFeed() {
     }
   }, [chat, isAutoScrollEnabled]);
 
-  // Handle manual scroll interactions
-  // const handleScroll = () => {
-  //   if (!scrollRef.current) return;
-  //   const { scrollTop, scrollHeight, clientHeight } = scrollRef.current;
-
-  //   // Disable auto-scroll if the user is not at the bottom
-  //   const isAtBottom = scrollTop + clientHeight >= scrollHeight - 10;
-  //   setIsAutoScrollEnabled(isAtBottom);
-  // };
-
   return (
     <div
-      className='feedContainer'
-      // ref={scrollRef}
-      // onScroll={handleScroll}
-      // style={{ overflowY: "auto", height: "600px" }}
+      className={`feedContainer ${isFullHeight ? "fullHeight" : "splitView"}`}
     >
       <div className='feed_header'>
-        <div className='close_icon'>
-          <svg
-            width='28'
-            height='25'
-            viewBox='0 0 22 19'
-            fill='none'
-            xmlns='http://www.w3.org/2000/svg'
-          >
-            <path
-              d='M7.84357 3.94482V5.75057C7.84357 6.06984 7.69538 6.37604 7.43159 6.6018C7.16781 6.82756 6.81004 6.95439 6.437 6.95439H4.32715M16.9863 6.95439H14.8764C14.5034 6.95439 14.1456 6.82756 13.8818 6.6018C13.618 6.37604 13.4698 6.06984 13.4698 5.75057V3.94482M13.4698 14.7793V12.9735C13.4698 12.6543 13.618 12.3481 13.8818 12.1223C14.1456 11.8965 14.5034 11.7697 14.8764 11.7697H16.9863M4.32715 11.7697H6.437C6.81004 11.7697 7.16781 11.8965 7.43159 12.1223C7.69538 12.3481 7.84357 12.6543 7.84357 12.9735V14.7793'
-              stroke='white'
-              stroke-width='1.81116'
-              stroke-linecap='round'
-              stroke-linejoin='round'
-            />
-          </svg>
+        <div
+          className='close_icon'
+          onClick={() => setIsFullHeight(!isFullHeight)}
+        >
+          <img src={closeIcon} />
         </div>
-      </div>
-      <div
-        className='listContainer'
-        style={{ height: "580px" }}
-        // ref={scrollRef}
-        // onScroll={handleScroll}
-        // style={{ overflowY: "auto", height: "580px" }}
-      >
-        {!isLoading && chat?.length === 0 ? (
-          <p>No data</p>
-        ) : page < 2 && isLoading ? (
-          loadingArray.map((_: any, i: number) => <p key={i}>Loading...</p>)
-        ) : (
-          <>
-            <VirtualizedContainer
-              listData={chat}
-              isLoading={isLoading}
-              page={page}
-              setPage={setPage}
-              limit={limit}
-              renderComponent={(index: number, chat: any) => (
-                // <div className='items' key={index}>
-                //   {chat.id}: {chat.message}
-                // </div>
-                <UserMessage
-                  key={index}
-                  userIcon={"https://via.placeholder.com/40"}
-                  userName={"0x0d2A...008631"}
-                  message={chat.message}
+        <span className='holder'> Holders Chat</span>
+        <div className='super_chat_container'>
+          <div className='s_chat_bx'>
+            <img
+              src='https://via.placeholder.com/40'
+              alt={`'s icon`}
+              className='icon'
+            />
+            <div className='chat_content'>
+              <span className='name'>0xgh...7897</span>
+              <span className='value'> $1000 </span>
+            </div>
+          </div>
+        </div>
+        <div>
+          {!isFullHeight && (
+            <div className='super_listContainer'>
+              {!isLoading && chat?.length === 0 ? (
+                <p>No data</p>
+              ) : (
+                <VirtualizedContainer
+                  listData={chat}
+                  isLoading={isLoading}
+                  page={page}
+                  setPage={setPage}
+                  limit={limit}
+                  renderComponent={(index: number, chat: any) => (
+                    <UserMessage
+                      key={index}
+                      userIcon={"https://via.placeholder.com/40"}
+                      userName={"0x0d2A...008631"}
+                      message={chat.message}
+                    />
+                  )}
+                  footerHeight={10}
                 />
               )}
-              footerHeight={10}
-            />
+            </div>
+          )}
+        </div>
+      </div>
 
-            {isLoading && page > 1 && <p>Loading...</p>}
-          </>
-        )}
+      <div className='chat_ox'>
+        <div className='normal_chat_container'>
+          {!isLoading && chat?.length === 0 ? (
+            <p>No data</p>
+          ) : page < 2 && isLoading ? (
+            loadingArray.map((_: any, i: number) => <p key={i}>Loading...</p>)
+          ) : (
+            <>
+              <VirtualizedContainer
+                listData={chat}
+                isLoading={isLoading}
+                page={page}
+                setPage={setPage}
+                limit={limit}
+                renderComponent={(index: number, chat: any) => (
+                  <UserMessage
+                    key={index}
+                    userIcon={"https://via.placeholder.com/40"}
+                    userName={"0x0d2A...008631"}
+                    message={chat.message}
+                  />
+                )}
+                footerHeight={10}
+              />
+
+              {isLoading && page > 1 && <p>Loading...</p>}
+            </>
+          )}
+        </div>
       </div>
     </div>
   );
--- src/components/aichat/userMessage/index.tsx
+++ src/components/aichat/userMessage/index.tsx
@@ -13,16 +13,34 @@ export default function UserMessage({
   message,
 }: UserMessageProps) {
   return (
-    <div className='user-message'>
-      <img
-        src={userIcon}
-        alt={`${userName}'s icon`}
-        className='user-message__icon'
-      />
-      <div className='user-message__content'>
-        <span className='user-message__name'>{userName}:</span>
-        <span className='user-message__text'>{message}</span>
+    <>
+      <div className='admin'>
+        <img
+          src={userIcon}
+          alt={`${userName}'s icon`}
+          className='user-message__icon'
+        />
+        <div className='admin__content'>
+          <span className='admin__name'>0xghfr...789776:</span>
+          <span className='admin__value'> $1000 </span>
+          <div className='admin__text'>
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            eiusmod tempor incididunt ut labore et dolore magna aliqua.
+          </div>
+        </div>
       </div>
-    </div>
+
+      <div className='user-message'>
+        <img
+          src={userIcon}
+          alt={`${userName}'s icon`}
+          className='user-message__icon'
+        />
+        <div className='user-message__content'>
+          <span className='user-message__name'>{userName}:</span>
+          <span className='user-message__text'>{message}</span>
+        </div>
+      </div>
+    </>
   );
 }
