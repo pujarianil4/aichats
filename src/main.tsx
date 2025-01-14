98dc04f2f084a029ad7ba47a3d4c35341be4396f
Anil Pujari <pujarianil4@gmail.com>
2025-01-14 20:48:38 +0530

    button changes

--- src/main.tsx
+++ src/main.tsx
@@ -86,6 +86,7 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
                 colorBgContainer: "#1c1c1c",
 
                 colorText: "#fffff",
+                boxShadow: "0px",
               },
               components: {
                 Table: {
