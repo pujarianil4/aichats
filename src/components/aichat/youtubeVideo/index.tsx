2e78418672cca1a18807378fa5219b2ef25f73f9
yugansh <ganurag@4waytechnologies.com>
2025-03-13 15:17:01 +0530

    added livestream code

--- src/components/aichat/youtubeVideo/index.tsx
+++ src/components/aichat/youtubeVideo/index.tsx
@@ -1,6 +1,18 @@
+import axios from "axios";
 import "./index.scss";
+import { useEffect, useState } from "react";
+
+interface IVideoIdResponse {
+  etag: string;
+  kind: string;
+  id: {
+    kind: string;
+    videoId: string;
+  };
+}
 
 export default function YoutubeVideo({ youtubeLink }: { youtubeLink: string }) {
+  const [videoId, setVideoId] = useState<string>();
   const extractYouTubeID = (url: string) => {
     const regex =
       /(?:youtube\.com\/(?:embed\/|watch\?v=|live\/)|youtu\.be\/)([a-zA-Z0-9_-]{11})/;
@@ -9,7 +21,20 @@ export default function YoutubeVideo({ youtubeLink }: { youtubeLink: string }) {
     return match ? match[1] : null;
   };
 
-  const videoId = extractYouTubeID(youtubeLink);
+  const getVideoId = async () => {
+    const response = await axios.get(
+      "https://chat-service-rq16.onrender.com/grp-intance/streamlink?id=UC-XtczUVraQxMuwjFXXDseQ"
+    );
+    if (response?.data?.items && response?.data?.items?.length > 0) {
+      const arrayOfLiveStreams: IVideoIdResponse[] = response.data.items;
+      //console.log(arrayOfLiveStreams[arrayOfLiveStreams.length - 1].id.videoId);
+      setVideoId(arrayOfLiveStreams[arrayOfLiveStreams.length - 1].id.videoId);
+    }
+  };
+
+  useEffect(() => {
+    getVideoId();
+  }, []);
 
   return (
     <div className='videoContainer'>
